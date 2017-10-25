64b03f3f1bfc529b562798c71fbb87c24d9ed28e
Gulfz <flog-fy@hotmail.com>
2017-10-25 17:22:00 +0700

    test branch dev

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 //Static resources server
 app.use(express.static(__dirname + ''));
 
-var server = app.listen(8081, function () {  
+var server = app.listen(8080, function () {  
     var port = server.address().port;
     console.log('Server running at port %s', port);
 });
--- js/code.js
+++ js/code.js
@@ -3,10 +3,10 @@ var main = { preload : preload , create: create , update : update};
 game.state.add('main', main);
 game.state.start('main');
 function preload() {
-    console.log("preload")
+    console.log("Hello")
 }
 function create() {
-    console.log("create")
+    console.log("World")
 }
 function update() {
     console.log("This is Monster Jump")
