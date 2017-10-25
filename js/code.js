d7b0ca7867612e65bfadeaded964f91fd5db3043
Gulfz <flog-fy@hotmail.com>
2017-10-25 17:15:29 +0700

    init project

---
+++ js/code.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(1500, 600, Phaser.AUTO, "game");
+var main = { preload : preload , create: create , update : update};
+game.state.add('main', main);
+game.state.start('main');
+function preload() {
+    console.log("preload")
+}
+function create() {
+    console.log("create")
+}
+function update() {
+    console.log("update")
+}
