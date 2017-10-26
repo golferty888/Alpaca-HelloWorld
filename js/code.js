be3652a3f5681514da66c33419cc73a3ae79276e
saguins <saralara555@gmail.com>
2017-10-26 15:20:16 +0700

    bouncefollowmouse

--- js/code.js
+++ js/code.js
@@ -1,13 +1,41 @@
-var game = new Phaser.Game(1500, 600, Phaser.AUTO, "game");
+var game = new Phaser.Game(300, 500, Phaser.AUTO, "game");
 var main = { preload : preload , create: create , update : update};
 game.state.add('main', main);
 game.state.start('main');
 function preload() {
-    console.log("Hello")
+    game.load.image('mamegoma','image/mamegoma.jpg')
+    game.load.image('window','image/window.jpg')
+    game.load.image('platfrom','image/platfrom.jpg')
 }
 function create() {
-    console.log("World")
+    game.add.sprite(0, 0, 'window')
+    // game.add.sprite(100,400,'platfrom')  
+    // game.add.sprite(0,0,'mamegoma')
+
+    game.physics.startSystem(Phaser.Physics.ARCADE)
+
+    char = game.add.sprite(game.world.centerX, game.world.centerY, 'mamegoma')
+    game.physics.enable(char, Phaser.Physics.ARCADE)
+    char.body.velocity.y = 300
+    char.body.collideWorldBounds = true
+    char.body.bounce.set(1)
+    char.body.gravity.y = 300
+
+    game.canvas.addEventListener('mousedown', requestLock);
+    game.input.addMoveCallback(move, this);
 }
+
+function requestLock() {
+    game.input.mouse.requestPointerLock();
+}
+
+function move(pointer, x, y, click) {
+    if (game.input.mouse.locked && !click)
+    {
+        char.x += game.input.mouse.event.movementX;
+    }
+}
+
 function update() {
-    console.log("This is Monster Jump")
+    
 }
